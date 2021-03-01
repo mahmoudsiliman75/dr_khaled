167d5727915e0186bb8727f95bce32d78e5d6278
mahmoudsiliman75 <mahmoudsiliman75@gmail.com>
2021-03-01 14:48:56 +0200

    finish navbar

--- layouts/TheHeader.php
+++ layouts/TheHeader.php
@@ -33,3 +33,65 @@
   <title> Dr.Khaled Salah </title>
 </head>
 <body>
+
+<!-- START:: LARGE SCREENS NAVBAR -->
+<nav class="d-flex justify-content-between align-items-center">
+  <!-- START:: THE LOGO -->
+  <div class="site-logo">
+    <img src="assets/media/logo.png" alt="The Logo">
+  </div>
+  <!-- END:: THE LOGO -->
+
+  <!-- START:: NAV LINKS -->
+  <div class="nav-links">
+    <ul class="list-unstyled d-flex align-items-center justify-content-center mb-0">
+      <li class="active"> 
+        <a href="index.php"> Home </a>
+      </li>
+
+      <li> 
+        <a href="#"> Dr.khaled Profile </a>
+      </li>
+
+      <li> 
+        <a href="#"> Operations </a>
+      </li>
+
+      <li> 
+        <a href="#"> How To Share With Us </a>
+      </li>
+
+      <li> 
+        <a href="#"> Books </a>
+      </li>
+
+      <li> 
+        <a href="#"> Cairo University </a>
+      </li>
+
+      <li> 
+        <a href="#"> Videos Liberary </a>
+      </li>
+
+      <li> 
+        <a href="#"> Login </a>
+      </li>
+
+      <li> 
+        <a href="#"> Contact Us </a>
+      </li>
+    </ul>
+  </div>
+  <!-- START:: NAV LINKS -->
+
+  <!-- START:: LANG BUTTON -->
+  <div class="lang-button">
+    <button> 
+      <span> E </span>
+      <span> ع </span>
+    </button>
+  </div>
+  <!-- END:: LANG BUTTON -->
+
+</nav>
+<!-- END:: LARGE SCREENS NAVBAR -->
