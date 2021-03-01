f3d9de017071f3018070e9c3d26f6362ad8e1faf
mahmoudsiliman75 <mahmoudsiliman75@gmail.com>
2021-03-01 12:13:39 +0200

    set the project setup

---
+++ layouts/TheFooter.php
@@ -0,0 +1,30 @@
+<!-- START:: INCLUDING JQUERY SCRIPT FILE -->
+<script src="assets/js/jquery.min.js"></script>
+<!-- END:: INCLUDING JQUERY SCRIPT FILE -->
+
+<!-- START:: INCLUDING BOOTSTRAP ASSETS SCRIPT FILE -->
+<script src="assets/js/pooper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<!-- END:: INCLUDING BOOTSTRAP ASSETS SCRIPT FILE -->
+
+<!-- START:: INCLUDING LIGHT BOX SCRIPT FILE -->
+<script src="assets/js/lightbox.js"></script>
+<!-- END:: INCLUDING LIGHT BOX SCRIPT FILE -->
+
+<!-- START:: INCLUDING FONTAWESOME SCRIPT FILE -->
+<script src="assets/js/all.min.js"></script>
+<!-- END:: INCLUDING FONTAWESOME SCRIPT FILE -->
+
+<!-- START:: INCLUDING OWL CAROUSEL SCRIPT FILE -->
+<script src="assets/js/owl.carousel.min.js"></script>
+<!-- END:: INCLUDING OWL CAROUSEL SCRIPT FILE -->
+
+<!-- START:: INCLUDING SITE FONT -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@400;500;600;700;900&display=swap">
+<!-- END:: INCLUDING SITE FONT -->
+
+<!-- START:: INCLUDING MAIN SCRIPT FILE -->
+<script src="assets/js/main.js"></script>
+<!-- END:: INCLUDING MAIN SCRIPT FILE -->
+</body>
+</html>
---
+++ layouts/TheHeader.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- START:: INCLUDING FONTAWESOME STYLE FILE -->
+  <link rel="stylesheet" href="assets/css/all.min.css">
+  <!-- END:: INCLUDING FONTAWESOME STYLE FILE -->
+
+  <!-- START:: INCLUDING  LIGHT BOX STYLE FILE -->
+  <link rel="stylesheet" href="assets/css/lightbox.css">
+  <!-- END:: INCLUDING  LIGHT BOX STYLE FILE -->
+
+  <!-- START:: INCLUDING OWL CAROUSEL STYLE FILES -->
+  <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+  <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+  <!-- END:: INCLUDING OWL CAROUSEL STYLE FILES -->
+
+  <!-- START:: INCLUDING BOOTSTRAP STYLE FILE -->
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <!-- END:: INCLUDING BOOTSTRAP STYLE FILE -->
+  
+  <!-- START:: INCLUDING MAIN STYLE FILE -->
+  <link rel="stylesheet" href="assets/css/style.css">
+  <!-- END:: INCLUDING MAIN STYLE FILE -->
+
+  <!-- START:: SET FAV ICON -->
+  <link rel="icon" href="assets/media/logo.png">
+  <!-- END:: SET FAV ICON -->
+
+  <title> Dr.Khaled Salah </title>
+</head>
+<body>
