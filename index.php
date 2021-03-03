ee4346faa7ee16482007e1789b27e52f7df8d74a
mahmoudsiliman75 <mahmoudsiliman75@gmail.com>
2021-03-03 11:43:30 +0200

    finish about section at home page

--- index.php
+++ index.php
@@ -2,7 +2,79 @@
 <?php include('./layouts/TheHeader.php')?>
 <!-- END:: INCLUDING THE HEADER -->
 
+<!-- START:: HOME SLIDER -->
+<div id="home_slider" class="carousel slide carousel-fade" data-ride="carousel">
 
+  <ol class="carousel-indicators">
+    <li data-target="#home_slider" data-slide-to="0" class="active"></li>
+    <li data-target="#home_slider" data-slide-to="1"></li>
+    <li data-target="#home_slider" data-slide-to="2"></li>
+    <li data-target="#home_slider" data-slide-to="3"></li>
+  </ol>
+
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="assets/media/pic1.jpg" class="d-block" alt="Slide 1">
+    </div>
+    <div class="carousel-item">
+      <img src="assets/media/pic2.jpg" class="d-block" alt="Slide 2">
+    </div>
+    <div class="carousel-item">
+      <img src="assets/media/pic3.jpg" class="d-block" alt="Slide 3">
+    </div>
+    <div class="carousel-item">
+      <img src="assets/media/pic4.jpg" class="d-block" alt="Slide 4">
+    </div>
+  </div>
+
+</div>
+<!-- END:: HOME SLIDER -->
+
+<!-- START::ABOUT US SECTION -->
+<div class="about_us_sec">
+  <div class="continer">
+    <div class="row justify-content-center p-2">
+      <div class="col-12 col-md-6 my-2 m-md-0 about_img_box">
+        <img src="assets/media/pic9.jpg" alt="About Operations">
+      </div>
+
+      <div class="col-12 col-md-6 my-2 m-md-0 about_img_box">
+        <img src="assets/media/pic8.jpg" alt="About Books">
+      </div>
+
+      <div class="col-12 col-md-11 mt-3">
+        <div class="about_card row">
+          <div class="col-12 col-md-6 about_card_img">
+            <img src="assets/media/pic5.jpg" alt="About ">
+          </div>
+
+          <div class="col-12 col-md-6 about_card_info">
+            <h3> Dr.Khaled Profile </h3>
+            <p class="my-3">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Voluptates blanditiis aut itaque placeat deleniti quidem 
+              ipsum amet tempora, porro quae delectus reiciendis! Odio 
+              dolore nihil omnis odit, ut necessitatibus ipsum.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Voluptates blanditiis aut itaque placeat deleniti quidem 
+              ipsum amet tempora, porro quae delectus reiciendis! Odio 
+              dolore nihil omnis odit, ut necessitatibus ipsum.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Voluptates blanditiis aut itaque placeat deleniti quidem 
+              ipsum amet tempora, porro quae delectus reiciendis! Odio 
+              dolore nihil omnis odit, ut necessitatibus ipsum.
+            </p>
+            <div class="btn_box">
+              <a href="DrProfile.php"> read more </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</div>
+<!-- START::ABOUT US SECTION -->
 
 <!-- START:: INCLUDING THE FOOTER -->
 <?php include('./layouts/TheFooter.php')?>
