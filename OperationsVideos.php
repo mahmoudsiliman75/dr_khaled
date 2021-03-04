ebeeb593024acb5a570c96b272b94c2df3e09e52
mahmoudsiliman75 <mahmoudsiliman75@gmail.com>
2021-03-04 10:52:28 +0200

    finish operation videos liberary

---
+++ OperationsVideos.php
@@ -0,0 +1,103 @@
+<!-- START:: INCLUDING THE HEADER -->
+<?php include('./layouts/TheHeader.php')?>
+<!-- END:: INCLUDING THE HEADER -->
+
+<!-- START:: HEDAER IMAGE SECTION -->
+<div class="header_img_wraper">
+  <img src="assets/media/pic14.jpg" alt="Intro Img">
+
+  <!-- START:: SECTION CURVE -->
+  <div class="section_svg">
+    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+      <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" class="shape-fill"></path>
+    </svg>
+  </div>
+  <!-- END:: SECTION CURVE -->
+</div>
+<!-- END:: HEDAER IMAGE SECTION -->
+
+<!-- START:: POERATION VIDEOS -->
+<div class="videos">
+  <div class="container">
+    <div class="header_wraper">
+      <h2> Operations Videos </h2>
+    </div>
+
+
+    <div class="row justify-content-center mt-3">
+      <div class="col-12 col-md-4 mb-2">
+        <div class="video_wraper">
+          <iframe src="https://www.youtube.com/embed/8udmtW-y8Qc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <h4> Video Title </h4>
+        </div>
+      </div>
+
+      <div class="col-12 col-md-4 mb-2">
+        <div class="video_wraper">
+          <iframe src="https://www.youtube.com/embed/8udmtW-y8Qc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <h4> Video Title </h4>
+        </div>
+      </div>
+
+      <div class="col-12 col-md-4 mb-2">
+        <div class="video_wraper">
+          <iframe src="https://www.youtube.com/embed/8udmtW-y8Qc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <h4> Video Title </h4>
+        </div>
+      </div>
+
+      <div class="col-12 col-md-4 mb-2">
+        <div class="video_wraper">
+          <iframe src="https://www.youtube.com/embed/8udmtW-y8Qc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <h4> Video Title </h4>
+        </div>
+      </div>
+
+      <div class="col-12 col-md-4 mb-2">
+        <div class="video_wraper">
+          <iframe src="https://www.youtube.com/embed/8udmtW-y8Qc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <h4> Video Title </h4>
+        </div>
+      </div>
+
+      <div class="col-12 col-md-4 mb-2">
+        <div class="video_wraper">
+          <iframe src="https://www.youtube.com/embed/8udmtW-y8Qc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <h4> Video Title </h4>
+        </div>
+      </div>
+
+      <div class="col-12 col-md-4 mb-2">
+        <div class="video_wraper">
+          <iframe src="https://www.youtube.com/embed/8udmtW-y8Qc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <h4> Video Title </h4>
+        </div>
+      </div>
+
+      <div class="col-12 col-md-4 mb-2">
+        <div class="video_wraper">
+          <iframe src="https://www.youtube.com/embed/8udmtW-y8Qc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <h4> Video Title </h4>
+        </div>
+      </div>
+
+      <div class="col-12 col-md-4 mb-2">
+        <div class="video_wraper">
+          <iframe src="https://www.youtube.com/embed/8udmtW-y8Qc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <h4> Video Title </h4>
+        </div>
+      </div>
+
+      <div class="col-12 mt-2">
+        <div class="btn_box">
+          <a href="#"> Load More </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- END:: POERATION VIDEOS -->
+
+<!-- START:: INCLUDING THE FOOTER -->
+<?php include('./layouts/TheFooter.php')?>
+<!-- END:: INCLUDING THE FOOTER -->
