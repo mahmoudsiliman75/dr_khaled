cfa8ce20e1f5dc74ca282bf22fb32eea6b8b560e
MahmoudSiliman <modysiliman75@gmail.com>
2021-03-05 18:39:58 +0200

    finish book info section at book details page

--- BookDetails.php
+++ BookDetails.php
@@ -18,13 +18,13 @@
 
 <!-- START:: BOOK INFO SECTIO -->
 <div class="book_info">
-	<div class="container">
+	<div class="container-fluid">
 		<div class="row">
 			<div class="col-12 col-md-6">
 				<div class="flexslider">
 					<ul class="slides">
-						<li data-thumb="assets/media/book_cover.jpg">
-							<img src="assets/media/book_cover.jpg" />
+						<li data-thumb="https://i.ytimg.com/vi/8udmtW-y8Qc/default.jpg">
+							<iframe src="https://www.youtube.com/embed/8udmtW-y8Qc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 						</li>
 
 						<li data-thumb="assets/media/certificate.png">
@@ -48,7 +48,66 @@
 					<h2> Name Of Book </h2>
 				</div>
 				<div class="book_text_wraper">
+					<div class="tabs_wraper">
+						<div class="row">
+							<ul class="col-5 nav nav-pills mb-3" id="pills-tab" role="tablist">
+								<li class="nav-item" role="presentation">
+									<a class="nav-link active" id="pills-pdf-tab" data-toggle="pill" href="#pills-pdf" role="tab" aria-controls="pills-pdf" aria-selected="true">PDF</a>
+								</li>
 
+								<li class="nav-item" role="presentation">
+									<a class="nav-link" id="pills-hard-tab" data-toggle="pill" href="#pills-hard" role="tab" aria-controls="pills-hard" aria-selected="false">Hard Copy</a>
+								</li>
+							</ul>
+
+							<div class="col-7 price">
+								<span class="old_price"> 300 EGP </span>
+								<span class="new_price"> 200 EGP </span>
+							</div>
+						</div>
+					</div>
+
+					<div class="tab-content mt-3" id="pills-tabContent">
+						<div class="tab-pane fade show active" id="pills-pdf" role="tabpanel" aria-labelledby="pills-pdf-tab">
+							(PDF) Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+							Deserunt rem perspiciatis fugit pariatur suscipit quos fugiat cumque at, 
+							maxime nisi obcaecati, fuga quasi voluptatibus architecto inventore 
+							sed repellat! Quia, cupiditate.
+							Deserunt rem perspiciatis fugit pariatur suscipit quos fugiat cumque at, 
+							maxime nisi obcaecati, fuga quasi voluptatibus architecto inventore 
+							sed repellat! Quia, cupiditate.
+							Deserunt rem perspiciatis fugit pariatur suscipit quos fugiat cumque at, 
+							maxime nisi obcaecati, fuga quasi voluptatibus architecto inventore 
+							sed repellat! Quia, cupiditate.
+						</div>
+
+						<div class="tab-pane fade" id="pills-hard" role="tabpanel" aria-labelledby="pills-hard-tab">
+							(Hard Copy) Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+							Deserunt rem perspiciatis fugit pariatur suscipit quos fugiat cumque at, 
+							maxime nisi obcaecati, fuga quasi voluptatibus architecto inventore 
+							sed repellat! Quia, cupiditate.
+							Deserunt rem perspiciatis fugit pariatur suscipit quos fugiat cumque at, 
+							maxime nisi obcaecati, fuga quasi voluptatibus architecto inventore 
+							sed repellat! Quia, cupiditate.
+							Deserunt rem perspiciatis fugit pariatur suscipit quos fugiat cumque at, 
+							maxime nisi obcaecati, fuga quasi voluptatibus architecto inventore 
+							sed repellat! Quia, cupiditate.
+						</div>
+					</div>
+
+					<div class="actions_wraper">
+						<div class="row justify-content-between">
+							<div class="col-4 d-flex justify-content-start">
+								<a href="#"> 
+									look <span> Inside </span> 
+									<i class="fas fa-search"></i>
+								</a>
+							</div>
+							<div class="col-4 d-flex justify-content-end">
+								<button> Add To Cart <i class="fas fa-cart-plus"></i> </button>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
